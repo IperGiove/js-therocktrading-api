b774cc455a8c1888c52e21a04e0467a5bc46a6b2
ipergiove <ipergiove@gmail.com>
2022-09-26 23:08:09 +0200

    test ohlc

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const { TheRockTrading } = require('./therocktrdaing');
 
 
-let trt = new TheRockTrading(API="API", API_SECRET="API_SECRET");
-console.log(trt)
+let trt = new TheRockTrading(API=process.env.TRT_API, API_SECRET=process.env.TRT_API_SECRET);
+//console.log(trt);
 
-let request_value = trt.currencies()
+let request_value = trt.ohlc_statistics(fund_id="BTCEUR", {"after": '2022-09-26T20:30:00.000Z', "period": 60});
 
 request_value.then(function(result) {
     console.log(result)
-})
+});
